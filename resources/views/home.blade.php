270a71eb88b24bf65433c5315ef36fc649f8185f
alexellevu <newnapster83@gmail.com>
2021-07-13 17:42:13 +0200

    aggiunta rotta +controller+inizio layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.app')
 
-        <title>Laravel</title>
+@section('title', 'Home Page')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <body>
-        <h1>laravel migration</h1>
-    </body>
-</html>
+@section ('content')
+
+@endsection
